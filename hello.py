0cbebd1cbb9a3bfa73917e2179343c66897c07f1
neo <zrhett@163.com>
2017-10-13 21:59:22 +0800

    a1

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-	return '<h1>Hello World!</h1>'
+def index():
+	user_agent = request.headers.get('User-Agent')
+	return '<p>Your browser is %s</p>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
