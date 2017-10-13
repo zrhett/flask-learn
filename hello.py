fb2a5e16fe6915b01296d82524a3c52f9e03e20b
neo <zrhett@163.com>
2017-10-13 22:43:10 +0800

    b1

--- hello.py
+++ hello.py
@@ -1,18 +1,17 @@
-from flask import Flask
-from flask import request
-
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
 app = Flask(__name__)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-	user_agent = request.headers.get('User-Agent')
-	return '<p>Your browser is %s</p>' % user_agent
+	return render_template('index.html')
+
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s!</h1>' % name
+	return render_template('user.html', name=name)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
-
