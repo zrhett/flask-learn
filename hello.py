995afbdb35c045772daf4d3c547aeea6a4ea910e
neo <zrhett@163.com>
2017-10-14 16:14:21 +0800

    b2

--- hello.py
+++ hello.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from datetime import datetime
 
-@app.route('/')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'unsecret! to secret ape?'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 
 @app.route('/user/<name>')
@@ -19,6 +32,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
+class NameForm(FlaskForm):
+	name = StringField('你叫什么名字？', validators=[DataRequired()])
+	submit = SubmitField('Submit')
 
 if __name__ == '__main__':
 	app.run(debug=True)
