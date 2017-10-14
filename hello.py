6ed71557ce0d58a53c711368ac68af0925a6c569
neo <zrhett@163.com>
2017-10-14 11:37:15 +0800

    b1

--- hello.py
+++ hello.py
@@ -12,6 +12,13 @@ def index():
 def user(name):
 	return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
 
 if __name__ == '__main__':
 	app.run(debug=True)
