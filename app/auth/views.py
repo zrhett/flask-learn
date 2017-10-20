2b6747facb0cb0bc8cfe2271869e150c1030466b
zrhett <zrhett@163.com>
2017-10-20 16:47:44 +0800

    P99, 下次该9.2 赋予角色

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, login_required
+from flask_login import login_user, login_required, logout_user
 from . import auth
 from ..models import User
 from .. import db
